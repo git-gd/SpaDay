10aaf94b0b82488972bae1e15388ea0c7776fb84
Randy Parker <randallbparker@gmail.com>
2020-11-11 08:56:15 -0600

    Added properties to User Model

--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -7,5 +7,8 @@ namespace SpaDay.Models
 {
     public class User
     {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
